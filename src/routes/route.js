b2e80fa9ec2a71d38f9abf40b67d62df69556c95
Ankit gupta <1729ankit@gmail.com>
2022-08-11 01:30:27 +0530

    hello

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const abc = require('../introduction/intro')
 
-const logger = require("../logger/logger.js")
-const helper = require("../util/helper")
-const validator = require("../validator/formetter.js")
+// const logger = require("../logger/logger.js")
+// const helper = require("../util/helper")
+// const validator = require("../validator/formetter.js")
 const lodash = require("lodash")
 const router = express.Router();
 router.get('/test-me', function (req, res) {
@@ -11,25 +11,27 @@ router.get('/test-me', function (req, res) {
     abc.printName()
     res.send('My second ever api!')
 
-    logger.welcome();
-    helper.printDate()
-    helper.month()
+    // logger.welcome();
+    // helper.printDate()
+    // helper.month()
 
-    helper.batchInfo()
-    validator.trimString()
-    validator.changetoLowerCase()
-    validator.changeToUpperCase()
-    const year = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec",
-    ];
-    const result = lodash.chunk(year, 4)
-    console.log(result)
-    const odd1 = [1, 3, 5, 7, 11, 13, 15, 17, 19, 21]
-    const result1 = lodash.tail(odd1)
-    console.log(result1)
+    // helper.batchInfo()
+    // validator.trimString()
+    // validator.changetoLowerCase()
+    // validator.changeToUpperCase()
 
-    const array4 = [['horror', 'The Shining'], ['drama', 'Titanic'], ['thriller', 'Shutter Island'], ['fantasy', 'Pans Labyrinth']]
-    const result4 = lodash.fromPairs(array4)
-    console.log(result4)
+
+    // const year = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec",
+    // ];
+    // const result = lodash.chunk(year, 4)
+    // console.log(result)
+    // const odd1 = [1, 3, 5, 7, 11, 13, 15, 17, 19, 21]
+    // const result1 = lodash.tail(odd1)
+    // console.log(result1)
+
+    // const array4 = [['horror', 'The Shining'], ['drama', 'Titanic'], ['thriller', 'Shutter Island'], ['fantasy', 'Pans Labyrinth']]
+    // const result4 = lodash.fromPairs(array4)
+    // console.log(result4)
 
 
 
