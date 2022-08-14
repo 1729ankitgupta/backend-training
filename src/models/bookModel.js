180756b7a59a87bce4b8850c3e8c43779d7209bf
Ankit gupta <1729ankit@gmail.com>
2022-08-14 23:41:41 +0530

    demo2

---
+++ src/models/bookModel.js
@@ -0,0 +1,22 @@
+/*====================================================assignment W4D5=================================================
+
+On similar lines as today's mongodb session ,  complete this assignment and submit explainer video for the same : a 
+book schema with bookname,  authorname , catageory , and year. create same 2 api for book i.e.: 1 api to create a new book
+and another api to get the  list of all books.*/
+
+const mongoose=require("mongoose");
+
+const bookSchema=new mongoose.Schema({
+    bookName:{
+        type:String,
+        unique:true,
+        require:true
+    }, 
+    authoreName:String,
+    category:{
+        type:String,
+        enum:["novel", "education", "stroy", "motivational"]
+    },
+    year:Number
+})
+module.exports=mongoose.model("book",bookSchema);
