23d50144b71e9fdad16a1b0ef1fc2c4e3e5f7d6a
Ankit gupta <1729ankit@gmail.com>
2022-08-30 15:49:49 +0530

    auth-1

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const route = require('./routes/route.js');
+var bodyParser = require('body-parser');
 const { default: mongoose } = require('mongoose');
+const route = require('./routes/route.js');
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://1729ankit:MSfnlOw77aSsseHm@cluster0.g9nthlr.mongodb.net/auth-1", {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+    .then(() => console.log("MongoDB is connected"))
+    .catch(err => console.log(err))
 
+app.use(
+    function (req, res, next) {
+        const date = new Date()
+        const ip = req.ip
+        const rout = req.originalUrl
+        //console.log(date,",",",",ip,rout)
+        next()
+    }
+)
 
-app.use('/', route)
-
+app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
