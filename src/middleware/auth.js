426e1fbc02b07f39dd9c16904382fa63d044d0a4
Ankit gupta <1729ankit@gmail.com>
2022-08-31 00:25:50 +0530

    auth2

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,20 @@
-const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
+const jwt = require('jsonwebtoken')
 
-    next()
+
+const validation = async function (req, res, next) {
+
+    let token = req.headers["x-auth-token"]
+    if (!token) {
+        return res.send({ status: false, msg: "token is mandatory" })
+    }
+    let decode = await jwt.verify(token, "arsh-plutonium")
+    if (!decode) {
+        return res.send({ status: false, msg: "This token is not valid" })
+    }
+    else {
+        next()
+    }
 }
 
 
-const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
-    next()
-}
+module.exports.validation = validation
