a74fe7269f0be43bda93d84ef874bf078a11a0d2
Ankit gupta <1729ankit@gmail.com>
2022-08-30 10:53:53 +0530

    middleware2

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const route = require('./routes/route.js');
+var bodyParser = require('body-parser');
 const { default: mongoose } = require('mongoose');
+const route = require('./routes/route.js');
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://1729ankit:MSfnlOw77aSsseHm@cluster0.g9nthlr.mongodb.net/middleware-2", {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+    .then(() => console.log("MongoDB is connected"))
+    .catch(err => console.log(err))
 
-
+app.use(
+    function (req, res, next) {
+        const date = new Date()
+        const ip = req.ip
+        const rout = req.originalUrl
+        //console.log(date,",",",",ip,rout)
+        next()
+    }
+)
 
 app.use('/', route);
-
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        res.send({msg:"done"})
-  }
-  );
-
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
---
+++ src/introduction/intro.js
@@ -0,0 +1,9 @@
+const batchName = "Plutonium"
+
+let printName = function () {
+    console.log('Bathc name is ', batchName)
+}
+
+module.exports.name = batchName
+module.exports.printName = printName
+
