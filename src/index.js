19df68aafcb015b95a358d677d4dfd15c61f6f43
Ankit gupta <1729ankit@gmail.com>
2022-08-31 05:59:17 +0530

    auth-2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://1729ankit:MSfnlOw77aSsseHm@cluster0.g9nthlr.mongodb.net/auth-2", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
